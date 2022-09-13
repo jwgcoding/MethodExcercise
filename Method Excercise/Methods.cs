d7249a29392548b9b93a79b629089fdbfcbc6512
Gary Li <jiaweigaryli@gmail.com>
2022-09-13 14:33:01 -0700

    redid code

---
+++ Method Excercise/Methods.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Excercise
+{
+    internal class Methods
+    {
+        public static void Excercise1()
+        {
+            Console.WriteLine("Tell me your name and I will tell you a story.");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("Tell me your age.");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Give me a dog name.");
+            var dogName = Console.ReadLine();
+
+            Console.WriteLine("What is ideal retirement age?");
+            int oldAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your favorite color?");
+            var favColor = Console.ReadLine();
+
+            Console.WriteLine("Where do you want to live?");
+            var location = Console.ReadLine();
+
+            Console.WriteLine($"There was a person named {userName}, who was {age} years old.");
+            Console.WriteLine($"{userName} lived with their dog, {dogName} whose fur was colored {favColor}.");
+            Console.WriteLine($"{userName} always dreamed of living in {location} until one day, they took the leap.");
+            Console.WriteLine($"It was everything {userName} had hoped for and stayed there and retired at the age of {oldAge}.");
+        }
+
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public static int Modulus(int x, int y)
+        {
+            return x % y;
+        }
+
+        public static int Sum(params int[] list)
+        {
+            int sum = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                sum += list[i];
+            }
+            return sum;
+        }
+#region Utility Methods
+        public static void AddBreak()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("***********************************************************************************************************************");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public static void AddSmallBreak()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("*******************************************************************");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        #endregion
+    }
+}
---
+++ Method Excercise/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Method_Excercise;
+Methods.Excercise1();
+
+Methods.AddBreak();
+
+//ADD
+Console.Write($"Addition Answer: ");
+Console.WriteLine(Methods.Add(20, 5));
+Methods.AddSmallBreak();
+
+Console.Write($"Subtraction Answer : ");
+Console.WriteLine(Methods.Subtract(30, 5));
+Methods.AddSmallBreak();
+
+Console.Write($"Multiplication Answer : ");
+Console.WriteLine(Methods.Multiply(5, 5));
+Methods.AddSmallBreak();
+
+Console.Write($"Division Answer: ");
+Console.WriteLine(Methods.Divide(125, 5));
+Methods.AddSmallBreak();
+
+Console.Write($"Modulus Answer: ");
+Console.WriteLine(Methods.Modulus(125,10));
+Methods.AddSmallBreak();
+
+Console.Write($"Sum(2,4) Answer: ");
+Console.WriteLine(Methods.Sum(2, 4));
+
+Console.Write($"Sum(2,4,6) Answer: ");
+Console.WriteLine(Methods.Sum(2, 4, 6));
+
+Console.Write($"Sum(1,1,1,1,1) Answer: ");
+Console.WriteLine(Methods.Sum(1, 1, 1, 1, 1));
